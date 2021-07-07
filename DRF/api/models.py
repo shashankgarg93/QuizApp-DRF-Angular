e7abd7bdf78dce6bd9933fb7bd0f394938efd126
Shiva Reddy <59078131+shiva-reddy-1001@users.noreply.github.com>
2021-07-07 17:59:43 +0530

    Model Framed

---
+++ DRF/api/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Question(models.Model) :
+    ques = models.CharField(max_length=100)
+    option1 = models.CharField(max_length=20)
+    option2 = models.CharField(max_length=20)
+    option3 = models.CharField(max_length=20)
+    option4 = models.CharField(max_length=20)
+    cans = models.CharField(
+        max_length=20,
+        choices=[(option1,'Choice 1'),(option2,'Choice 2'),(option3,'Choice 3'),(option4,'Choice 4')]
+    )
+
+    def __str__(self) -> str:
+        return self.ques
